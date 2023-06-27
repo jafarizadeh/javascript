82015762aea23e8744fc3d8f19210edaea7afe39
Mehdi <mehdi0341@gmail.com>
2023-06-27 10:13:04 +0200

    load images

--- Memory_Game/app.js
+++ Memory_Game/app.js
@@ -1,6 +1,50 @@
 const cardArray = [
-    {
-        name : 'fries',
-        img : 
-    }
-]
+  {
+    name: "fries",
+    img: "img/fries.jpg",
+  },
+  {
+    name: "burger",
+    img: "img/burger.png",
+  },
+  {
+    name: "hotdog",
+    img: "img/hotdog.png",
+  },
+  {
+    name: "ice_cream",
+    img: "img/ice_cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "img/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "img/pizza.png",
+  },
+  {
+    name: "fries",
+    img: "img/fries.jpg",
+  },
+  {
+    name: "burger",
+    img: "img/burger.png",
+  },
+  {
+    name: "hotdog",
+    img: "img/hotdog.png",
+  },
+  {
+    name: "ice_cream",
+    img: "img/ice_cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "img/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "img/pizza.png",
+  },
+];
