69e3f9f8d4569cbc1af6a32f64019898f604e0cc
Mehdi <mehdi0341@gmail.com>
2023-06-29 00:00:29 +0200

    for loop blocks

--- Breakout_Game/app.js
+++ Breakout_Game/app.js
@@ -5,19 +5,25 @@ const blockHeight = 20;
 // create Block
 class Block {
   constructor(xAxis, yAxis) {
-    this.topLeft = [xAxis, yAxis];
-    this.topRight = [xAxis + blockWidth, yAxis];
-    this.bottomLeft = [xAxis, yAxis + blockHeight];
-    this.bottomRight = [xAxis + blockWidth, yAxis + blockHeight];
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topLeft = [xAxis, yAxis + blockHeight];
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
   }
 }
-// draw my block
-function addBlock() {
-  const block = document.createElement("div");
-  block.classList.add("block");
-  grid.appendChild(block);
-  block.style.left = "100px";
-  block.style.bottom = "50px";
+
+// all my block
+const blocks = [new Block(10, 270)];
+
+// draw all my block
+function addBlocks() {
+  for (let i = 0; i < blocks.length; i++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    grid.appendChild(block);
+    block.style.left = blocks[i].bottomLeft[0] + "px";
+    block.style.bottom = blocks[i].bottomLeft[1] + "px";
+  }
 }
 
-addBlock();
+addBlocks();
