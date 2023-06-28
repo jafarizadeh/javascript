c5a0dffac760b5b6e89d2dc3d17906800e69fd92
Mehdi <mehdi0341@gmail.com>
2023-06-28 23:46:14 +0200

    create Block (Class)

--- Breakout_Game/app.js
+++ Breakout_Game/app.js
@@ -1,5 +1,16 @@
 const grid = document.querySelector(".grid");
+const blockWidth = 100;
+const blockHeight = 20;
 
+// create Block
+class Block {
+  constructor(xAxis, yAxis) {
+    this.topLeft = [xAxis, yAxis];
+    this.topRight = [xAxis + blockWidth, yAxis];
+    this.bottomLeft = [xAxis, yAxis + blockHeight];
+    this.bottomRight = [xAxis + blockWidth, yAxis + blockHeight];
+  }
+}
 // draw my block
 function addBlock() {
   const block = document.createElement("div");
