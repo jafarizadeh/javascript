32446545001d8fddf92cc3254a3f628ba02059ee
Mehdi <mehdi0341@gmail.com>
2023-06-28 16:32:09 +0200

    finish

--- Whac_a_mole/app.js
+++ Whac_a_mole/app.js
@@ -4,6 +4,9 @@ const timeLeft = document.querySelector("#time_left");
 const score = document.querySelector("#score");
 
 let result = 0;
+let hitPosition;
+let currentTime = 10;
+let timerId = null;
 
 function randomSquare() {
   squares.forEach((square) => {
@@ -12,11 +15,33 @@ function randomSquare() {
 
   let randomSquare = squares[Math.floor(Math.random() * 9)];
   randomSquare.classList.add("mole");
+  hitPosition = randomSquare.id;
 }
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id == hitPosition) {
+      result++;
+      score.textContent = result;
+      hitPosition = null;
+    }
+  });
+});
 
 function moveMole() {
-  let timerId = null;
-  timerId = setInterval(randomSquare, 500);
+  timerId = setInterval(randomSquare, 1000);
 }
 
 moveMole();
+
+function countDown() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+
+  if (currentTime == 0) {
+    clearInterval(countDownTimerId);
+    clearInterval(timerId);
+    alert("Game Over! Your final score is :" + result);
+  }
+}
+
+let countDownTimerId = setInterval(countDown, 1000);
