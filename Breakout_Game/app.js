2365dcb1a15ee3e37da8bc864148cef5e3fb92d5
Mehdi <mehdi0341@gmail.com>
2023-06-29 17:00:27 +0200

    move function

--- Breakout_Game/app.js
+++ Breakout_Game/app.js
@@ -48,7 +48,26 @@ addBlocks();
 // add user
 const user = document.createElement("div");
 user.classList.add("user");
-user.style.left = currentPosition[0] + "px";
-user.style.bottom = currentPosition[1] + "px";
-
+drawUser();
 grid.appendChild(user);
+
+// draw the user
+function drawUser() {
+  user.style.left = currentPosition[0] + "px";
+  user.style.bottom = currentPosition[1] + "px";
+}
+
+// move user
+function moveUser(e) {
+  switch (e.key) {
+    case "ArrowLeft":
+      currentPosition[0] -= 20;
+      drawUser();
+      break;
+    case "ArrowRight":
+      currentPosition[0] += 20;
+      drawUser();
+      break;
+  }
+}
+document.addEventListener("keydown", moveUser);
