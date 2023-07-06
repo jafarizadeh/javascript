e7693fcc2e11bd0be209b6201676ff0b890d892a
Mehdi <mehdi0341@gmail.com>
2023-07-06 22:47:43 +0200

    move player

--- Breakout/app.js
+++ Breakout/app.js
@@ -7,12 +7,14 @@ let context;
 // player;
 let playerWidth = 80;
 let playerHeight = 10;
+let playerVelocityX = 10;
 
 let player = {
   x: boardWidth / 2 - playerWidth / 2,
   y: boardHeight - playerHeight - 5,
   width: playerWidth,
   height: playerHeight,
+  velocityX: playerVelocityX,
 };
 
 window.onload = function () {
@@ -26,12 +28,22 @@ window.onload = function () {
   context.fillRect(player.x, player.y, player.width, player.height);
 
   requestAnimationFrame(update);
+  document.addEventListener("keydown", movePlayer);
 };
 
 function update() {
   requestAnimationFrame(update);
+  context.clearRect(0, 0, board.width, board.height);
 
   //player
   context.fillStyle = "lightgreen";
   context.fillRect(player.x, player.y, player.width, player.height);
+}
+
+function movePlayer(e) {
+  if (e.code == "ArrowLeft") {
+    player.x -= player.velocityX;
+  } else if (e.code == "ArrowRight") {
+    player.x += player.velocityX;
+  }
 }
