5855c30307ecb3c74b9f7b1cb088bb08d70c946a
Mehdi <mehdi0341@gmail.com>
2023-07-06 22:40:45 +0200

    game loop

--- Breakout/app.js
+++ Breakout/app.js
@@ -4,9 +4,34 @@ let boardWidth = 500;
 let boardHeight = 500;
 let context;
 
+// player;
+let playerWidth = 80;
+let playerHeight = 10;
+
+let player = {
+  x: boardWidth / 2 - playerWidth / 2,
+  y: boardHeight - playerHeight - 5,
+  width: playerWidth,
+  height: playerHeight,
+};
+
 window.onload = function () {
   board = document.getElementById("board");
   board.height = boardHeight;
   board.width = boardWidth;
   context = board.getContext("2d"); // used for drawing on the board
+
+  //draw initial player
+  context.fillStyle = "lightgreen";
+  context.fillRect(player.x, player.y, player.width, player.height);
+
+  requestAnimationFrame(update);
 };
+
+function update() {
+  requestAnimationFrame(update);
+
+  //player
+  context.fillStyle = "lightgreen";
+  context.fillRect(player.x, player.y, player.width, player.height);
+}
