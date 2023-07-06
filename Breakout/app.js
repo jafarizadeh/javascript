0c6fbbd7937dab1832ec100672b4fc00f1eed43e
Mehdi <mehdi0341@gmail.com>
2023-07-06 23:54:40 +0200

    ball move

--- Breakout/app.js
+++ Breakout/app.js
@@ -7,7 +7,7 @@ let context;
 // player;
 let playerWidth = 80;
 let playerHeight = 10;
-let playerVelocityX = 10;
+let playerVelocityX = 15;
 
 let player = {
   x: boardWidth / 2 - playerWidth / 2,
@@ -15,6 +15,21 @@ let player = {
   width: playerWidth,
   height: playerHeight,
   velocityX: playerVelocityX,
+};
+
+// ball
+let ballWidth = 10;
+let ballHeight = 10;
+let ballVelocityX = 3;
+let ballVelocityY = 2;
+
+let ball = {
+  x: boardWidth / 2,
+  y: boardHeight / 2,
+  width: ballWidth,
+  height: ballHeight,
+  velocityX: ballVelocityX,
+  velocityY: ballVelocityY,
 };
 
 window.onload = function () {
@@ -38,6 +53,24 @@ function update() {
   //player
   context.fillStyle = "lightgreen";
   context.fillRect(player.x, player.y, player.width, player.height);
+
+  //ball
+  context.fillStyle = "white";
+  ball.x += ball.velocityX;
+  ball.y += ball.velocityY;
+  context.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+  // bounce ball off walls
+  if (ball.y <= 0) {
+    // if ball touches top of canvas
+    ball.velocityY *= -1;
+  } else if (ball.x <= 0 || ball.x + ball.width >= boardWidth) {
+    // if ball touches left or right of canvas
+    ball.velocityX *= -1;
+  } else if (ball.y + ball.height >= boardHeight) {
+    // if ball touches bottom of canvas
+    // game over
+  }
 }
 
 function outPlayer(xPosition) {
@@ -58,4 +91,36 @@ function movePlayer(e) {
       player.x = nextPlayerX;
     }
   }
+  // bounce the ball off player paddle
+  if (topCollision(ball, player)) {
+    ball.velocityY *= -1;
+  } else if (leftCollision(ball, player) || rightCollision(ball, player)) {
+    ball.velocityX *= -1;
+  }
+}
+
+function detectCollision(a, b) {
+  return (
+    a.x < b.x + b.width && // a's top left corner doesn't reach b's top right corner
+    a.x + a.width > b.x && // a's top right corner passes b's top left corner
+    a.y < b.y + b.height && // a's top left corner doesn't reach b's bottom right corner
+    a.y + a.height > b.y // a's bottom left corner passes b's top left corner
+  );
+}
+
+function topCollision(ball, block) {
+  // a is above b (ball is above block)
+  return detectCollision(ball, block) && ball.y + ball.height >= block.y;
+}
+
+function bottomCollision(ball, block) {
+  return detectCollision(ball, block) && block.y + block.height >= ball.y;
+}
+
+function leftCollision(ball, block) {
+  return detectCollision(ball, block) && ball.x + ball.width >= block.x;
+}
+
+function rightCollision(ball, block) {
+  return detectCollision(ball, block) && block.x + block.width >= ball.x;
 }
