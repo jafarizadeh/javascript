7880a10d165e54dab236b2fdf9d8df021825af40
Mehdi <mehdi0341@gmail.com>
2023-07-07 12:12:19 +0200

    draw bird

---
+++ Flappy-Bird/ap.js
@@ -0,0 +1,37 @@
+// board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+// bird
+let birdWidth = 34;
+let birdHeight = 24;
+let birdX = boardWidth / 8;
+let birdY = boardHeight / 2;
+let birdImg;
+
+let bird = {
+  x: birdX,
+  y: birdY,
+  width: birdWidth,
+  height: birdHeight,
+};
+
+window.onload = function () {
+  board = document.getElementById("board");
+  board.height = boardHeight;
+  board.width = boardWidth;
+  context = board.getContext("2d");
+
+  // draw flappy bird
+//   context.fillStyle = "green";
+//   context.fillRect(bird.x, bird.y, bird.width, bird.height);
+
+  // load images
+  birdImg = new Image();
+  birdImg.src = "./img/flappybird.png";
+  birdImg.onload = function () {
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+  };
+};
