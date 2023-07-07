a478c0a22cb3d637676d8e96f1b8378ea599b95e
Mehdi <mehdi0341@gmail.com>
2023-07-07 15:37:51 +0200

    finish

--- Flappy-Bird/ap.js
+++ Flappy-Bird/ap.js
@@ -30,6 +30,11 @@ let bottomPipeImg;
 
 //physics
 let velocityX = -2; // pipes moving left speed
+let velocityY = 0; // bird jump speed
+let gravity = 0.4;
+
+let gameOver = false;
+let score = 0;
 
 window.onload = function () {
   board = document.getElementById("board");
@@ -56,20 +61,52 @@ window.onload = function () {
 
   requestAnimationFrame(update);
   setInterval(placePipe, 1500);
+  document.addEventListener("keydown", moveBird);
 };
 
 function update() {
+  if (gameOver) {
+    return;
+  }
   requestAnimationFrame(update);
   context.clearRect(0, 0, board.width, board.height);
 
   // bird
+  velocityY += gravity;
+  //   bird.y += velocityY;
+  bird.y = Math.max(bird.y + velocityY, 0); // apply gravity to current bird.y, limit the bird.y to top of the canvas
   context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+  if (bird.y > board.height) {
+    gameOver = true;
+  }
 
   // pipes
   for (let i = 0; i < pipeArray.length; i++) {
     let pipe = pipeArray[i];
     pipe.x += velocityX;
     context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+
+    if (!pipe.passed && bird.x > pipe.x + pipe.width) {
+      score += 0.5; // there are 2 pipes!
+      pipe.passed = true;
+    }
+
+    if (detectCollision(bird, pipe)) {
+      gameOver = true;
+    }
+  }
+
+  // clear pipes
+  while (pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) {
+    pipeArray.shift();
+  }
+  // score
+  context.fillStyle = "white";
+  context.font = "45px sans-serif";
+  context.fillText(score, 5, 45);
+
+  if (gameOver) {
+    context.fillText("GAME OVER", 5, 90);
   }
 }
 
@@ -97,4 +134,27 @@ function placePipe() {
     passed: false,
   };
   pipeArray.push(bottomPipe);
+}
+
+function moveBird(e) {
+  if (e.code == "Space" || e.code == "ArrowUp") {
+    // jump
+    velocityY = -6;
+    if (gameOver) {
+      requestAnimationFrame(update);
+      bird.y = birdY;
+      pipeArray = [];
+      score = 0;
+      gameOver = false;
+    }
+  }
+}
+
+function detectCollision(a, b) {
+  return (
+    a.x < b.x + b.width &&
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y
+  );
 }
