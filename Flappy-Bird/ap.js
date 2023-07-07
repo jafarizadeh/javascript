30a3f9b9130864c03fdfadabab8e3d548a2c9777
Mehdi <mehdi0341@gmail.com>
2023-07-07 12:52:28 +0200

    pipes moving

--- Flappy-Bird/ap.js
+++ Flappy-Bird/ap.js
@@ -18,6 +18,19 @@ let bird = {
   height: birdHeight,
 };
 
+// pipes
+let pipeArray = [];
+let pipeWidth = 64;
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+let topPipeImg;
+let bottomPipeImg;
+
+//physics
+let velocityX = -2; // pipes moving left speed
+
 window.onload = function () {
   board = document.getElementById("board");
   board.height = boardHeight;
@@ -25,8 +38,8 @@ window.onload = function () {
   context = board.getContext("2d");
 
   // draw flappy bird
-//   context.fillStyle = "green";
-//   context.fillRect(bird.x, bird.y, bird.width, bird.height);
+  //   context.fillStyle = "green";
+  //   context.fillRect(bird.x, bird.y, bird.width, bird.height);
 
   // load images
   birdImg = new Image();
@@ -34,4 +47,41 @@ window.onload = function () {
   birdImg.onload = function () {
     context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
   };
+
+  topPipeImg = new Image();
+  topPipeImg.src = "./img/toppipe.png";
+
+  bottomPipeImg = new Image();
+  bottomPipeImg.src = "./img/bottompipe.png";
+
+  requestAnimationFrame(update);
+  setInterval(placePipe, 1500);
 };
+
+function update() {
+  requestAnimationFrame(update);
+  context.clearRect(0, 0, board.width, board.height);
+
+  // bird
+  context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+  // pipes
+  for (let i = 0; i < pipeArray.length; i++) {
+    let pipe = pipeArray[i];
+    pipe.x += velocityX;
+    context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+  }
+}
+
+function placePipe() {
+  let topPipe = {
+    img: topPipeImg,
+    x: pipeX,
+    y: pipeY,
+    width: pipeWidth,
+    height: pipeHeight,
+    passed: false,
+  };
+
+  pipeArray.push(topPipe);
+}
