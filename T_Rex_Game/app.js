abfb95a7e9f746b702a25087c3cd199b2aa939b3
Mehdi <mehdi0341@gmail.com>
2023-07-10 16:28:09 +0200

    draw dino

--- T_Rex_Game/app.js
+++ T_Rex_Game/app.js
@@ -1,12 +1,12 @@
 //board
 let board;
-let boardWidth = 1125;  //750
+let boardWidth = 1125; //750
 let boardHeight = 375; //250
 let context;
 
 //dino
-let dinoWidth = 132; //88
-let dinoHeight = 141; //94
+let dinoWidth = 88;
+let dinoHeight = 94;
 let dinoX = 75; //50
 let dinoY = boardHeight - dinoHeight;
 let dinoImg;
@@ -22,4 +22,15 @@ window.onload = function () {
   board = document.getElementById("board");
   board.height = boardHeight;
   board.width = boardWidth;
+
+  context = board.getContext("2d"); //used for drawing on the board
+
+  //draw initial dinosaur
+  //   context.fillStyle = "green";
+  //   context.fillRect(dino.x, dino.y, dino.width, dino.height);
+  dinoImg = new Image();
+  dinoImg.src = "./img/dino.png";
+  dinoImg.onload = function(){
+  	context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+  }
 };
