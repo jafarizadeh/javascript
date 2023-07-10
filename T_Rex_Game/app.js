4472465ee7839ccb9b36019777dc83736a368bdb
Mehdi <mehdi0341@gmail.com>
2023-07-10 19:41:02 +0200

    moving cactus

--- T_Rex_Game/app.js
+++ T_Rex_Game/app.js
@@ -18,6 +18,29 @@ let dino = {
   height: dinoHeight,
 };
 
+//cactus
+let cactusArray = [];
+
+let cactus1Width = 34;
+let cactus2Width = 69;
+let cactus3Width = 102;
+
+let cactusHeight = 70;
+let cactusX = 1100;
+let cactusY = boardHeight - cactusHeight;
+
+let cactus1Img;
+let cactus2Img;
+let cactus3Img;
+
+//phisics
+let velocityX = -8; //cactus moving left speed
+let velocityY = 0;
+let gravity = 0.4;
+
+let gameOver = false;
+let score = 0;
+
 window.onload = function () {
   board = document.getElementById("board");
   board.height = boardHeight;
@@ -30,7 +53,75 @@ window.onload = function () {
   //   context.fillRect(dino.x, dino.y, dino.width, dino.height);
   dinoImg = new Image();
   dinoImg.src = "./img/dino.png";
-  dinoImg.onload = function(){
-  	context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
-  }
+  dinoImg.onload = function () {
+    context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+  };
+
+  cactus1Img = new Image();
+  cactus1Img.src = "./img/cactus1.png";
+
+  cactus2Img = new Image();
+  cactus2Img.src = "./img/cactus2.png";
+
+  cactus3Img = new Image();
+  cactus3Img.src = "./img/cactus3.png";
+
+  requestAnimationFrame(update);
+  setInterval(placeCactus, 1000);
 };
+
+function update() {
+  requestAnimationFrame(update);
+
+  context.clearRect(0, 0, board.width, board.height);
+
+  //dino
+  context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+
+  //cactus
+  for (let i = 0; i < cactusArray.length; i++) {
+    let cactus = cactusArray[i];
+    cactus.x += velocityX;
+    context.drawImage(
+      cactus.img,
+      cactus.x,
+      cactus.y,
+      cactus.width,
+      cactus.height
+    );
+  }
+}
+
+function placeCactus() {
+  //place cactus
+  let cactus = {
+    img: null,
+    x: cactusX,
+    y: cactusY,
+    width: null,
+    height: cactusHeight,
+  };
+
+  let placeCactusChance = Math.random();
+
+  if (placeCactusChance > 0.9) {
+    //10% chance
+    cactus.img = cactus3Img;
+    cactus.width = cactus3Width;
+    cactusArray.push(cactus);
+  } else if (placeCactusChance > 0.7) {
+    //30% chance
+    cactus.img = cactus2Img;
+    cactus.width = cactus2Width;
+    cactusArray.push(cactus);
+  } else if (placeCactusChance > 0.5) {
+    //50% chance
+    cactus.img = cactus1Img;
+    cactus.width = cactus1Width;
+    cactusArray.push(cactus);
+  }
+
+  if (cactusArray.length > 5) {
+    cactusArray.shift(); //remove the first element from the array so that the array doesn't constantly grow
+  }
+}
